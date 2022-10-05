906e5669086a37a24a5091f3330fca591c11273b
Ev-geniia <moonlitcat@inbox.ru>
2022-10-05 15:58:46 +0300

    added task 1 lesson 5

--- src/test/java/github/WikiSoftAssertions.java
+++ src/test/java/github/WikiSoftAssertions.java
@@ -1,10 +1,9 @@
 package github;
 
-import com.codeborne.selenide.Selenide;
+
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byLinkText;
 import static com.codeborne.selenide.Selenide.*;
 
